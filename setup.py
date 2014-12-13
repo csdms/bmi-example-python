c333b443f85b9f3dc3a8526c65ef71a88b8517b6
mcflugen <mcflugen@gmail.com>
2014-12-12 20:31:40 -0700

    Added setup.py and ez_setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+
+setup(name='bmi-poisson',
+      version='0.1.0',
+      author='Eric Hutton',
+      author_email='eric.hutton@colorado.edu',
+      description='BMI Python example',
+      long_description=open('README.md').read(),
+      packages=find_packages(),
+)
