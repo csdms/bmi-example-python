9de40615c7a42092f805874a15576fb2c5f7a6ef
Mark Piper <mark.piper@colorado.edu>
2015-03-17 11:29:13 -0600

    Fill out descriptions for non-grid methods

--- bmi/bmi.py
+++ bmi/bmi.py
@@ -18,22 +18,21 @@ class Bmi(object):
 
         Initialize() performs all tasks that take place before
         entering the model's time loop, including opening files and
-        initializing the model state. Models should be refactored, if
-        necessary, to read their inputs from a text-based
-        configuration file, specified by `filename`. CSDMS does not
-        impose any constraint on how configuration files are formatted
-        (although YAML is recommended). A "template" of a model's
-        configuration file with placeholder values is used by the BMI.
+        initializing the model state. Model inputs are read from a
+        text-based configuration file, specified by `filename`.
 
         Parameters
         ----------
         filename : str, optional
           The path to the model configuration file.
 
-        Returns
-        -------
-        int
-          Non-zero value indicates error code, or zero on success.
+        Notes
+        -----
+        Models should be refactored, if necessary, to use a
+        configuration file. CSDMS does not impose any constraint on
+        how configuration files are formatted, although YAML is
+        recommended. A template of a model's configuration file
+        with placeholder values is used by the BMI.
 
         """
         pass
@@ -48,21 +47,36 @@ class Bmi(object):
         computed by the ***initialize()*** method and this method can
         return with no action.
 
-        Returns
-        -------
-        int
-          Non-zero value indicates error code, or zero on success.
-
         """
         pass
 
     def update_until(self, time):
-        """Advances model state until the given time step.
+        """Advances model state until the given time.
+
+        Parameters
+        ----------
+        time : double
+          A model time value.
+
+        See Also
+        --------
+        update
+
         """
         pass
 
     def update_frac(self, time_frac):
         """Advances model state by a fraction of a time step.
+
+        Parameters
+        ----------
+        time_frac : double
+          A fraction of a model time step value.
+
+        See Also
+        --------
+        update
+
         """
         pass
 
@@ -73,98 +87,289 @@ class Bmi(object):
         the model's time loop. This typically includes deallocating
         memory, closing files and printing reports.
 
-        Returns
-        -------
-        int
-          Non-zero value indicates error code, or zero on success.
-
         """
         pass
 
     def get_component_name(self):
         """Returns the name of the component.
+
+        Returns
+        -------
+        str
+          The name of the component.
+
         """
         pass
 
     def get_input_var_names(self):
         """Lists the model's input variables.
+
+        Input variable names must be CSDMS Standard Names, also known
+        as *long variable names*.
+
+        Returns
+        -------
+        list of str
+          The input variables for the model.
+
+        Notes
+        -----
+        Standard Names enable the CSDMS framework to determine whether
+        an input variable in one model is equivalent to, or compatible
+        with, an output variable in another model. This allows the
+        framework to automatically connect components.
+
+        Standard Names do not have to be used within the model.
+
         """
         pass
 
     def get_output_var_names(self):
         """Lists the model's output variables.
+
+        Output variable names must be CSDMS Standard Names, also known
+        as *long variable names*.
+
+        Returns
+        -------
+        list of str
+          The output variables for the model.
+
+        See Also
+        --------
+        get_input_var_names
+
         """
         pass
 
     def get_var_type(self, long_var_name):
         """Returns the type of the given variable.
+
+        Parameters
+        ----------
+        long_var_name : str
+          An input or output variable name, a CSDMS Standard Name.
+
+        Returns
+        -------
+        str
+          The Python variable type; e.g., `str`, `int`, `float`.
+
         """
         pass
 
     def get_var_units(self, long_var_name):
         """Returns the units of the given variable.
+
+        Standard unit names, in lower case, should be used, such as
+        "meters" or "seconds". Standard abbreviations, like "m" for
+        meters, are also supported. For variables with compound units,
+        each unit name is separated by a single space, with exponents
+        other than 1 placed immediately after the name, as in "m s-1"
+        for velocity, "W m-2" for an energy flux, or "km2" for an
+        area.
+
+        Parameters
+        ----------
+        long_var_name : str
+          An input or output variable name, a CSDMS Standard Name.
+
+        Returns
+        -------
+        str
+          The variable units.
+
+        Notes
+        -----
+        CSDMS uses the UDUNITS standard from Unidata.
+
         """
         pass
 
     def get_var_rank(self, long_var_name):
         """Returns the number of dimensions of the given variable.
+
+        Scalars have a rank of 0, vectors a rank of 1, planar grids
+        and meshes a rank of 2, and 3D grids and meshes a rank of 3.
+
+        Parameters
+        ----------
+        long_var_name : str
+          An input or output variable name, a CSDMS Standard Name.
+
+        Returns
+        -------
+        int
+          The variable rank.
+
         """
         pass
 
     def get_var_size(self, long_var_name):
         """Returns the number of elements in the given variable.
+
+        Parameters
+        ----------
+        long_var_name : str
+          An input or output variable name, a CSDMS Standard Name.
+
+        Returns
+        -------
+        int
+          The count of elements in the variable.
+
         """
         pass
 
     def get_var_nbytes(self, long_var_name):
         """Returns the size, in bytes, of the given variable.
+
+        Parameters
+        ----------
+        long_var_name : str
+          An input or output variable name, a CSDMS Standard Name.
+
+        Returns
+        -------
+        int
+          The size of the variable, counted in bytes.
+
         """
         pass
 
     def get_start_time(self):
         """Returns the start time of the model.
+
+        Model times should be of type float. The default model start
+        time is 0.
+
+        Returns
+        -------
+        float
+          The model start time.
+
         """
         pass
 
     def get_current_time(self):
         """Returns the model's current time.
+
+        Returns
+        -------
+        float
+          The current model time.
+
+        See Also
+        --------
+        get_start_time
+
         """
         pass
 
     def get_end_time(self):
-        """Returns the end time of the model.
+        """Returns the maximum time of the model.
+
+        Returns
+        -------
+        float
+          The maximum model time.
+
+        See Also
+        --------
+        get_start_time
+
         """
         pass
 
     def get_time_step(self):
         """Returns the model's current time step.
+
+        The model time step should be of type float. The default time
+        step is 1.0.
+
+        Returns
+        -------
+        float
+          The time step used in model.
+
         """
         pass
 
     def get_time_units(self):
         """Returns the model's time units.
+
+        Returns
+        -------
+        float
+          The model time unit; e.g., 'days' or 's'.
+
+        See Also
+        --------
+        get_var_units
+
         """
         pass
 
     def get_value(self, long_var_name):
         """Returns the value of the given variable.
+
+        This is the getter for the model, used to access the model's
+        current state. It returns the value of a model variable, with
+        the return type, size and rank dependent on the variable.
+
+        Parameters
+        ----------
+        long_var_name : str
+          An input or output variable name, a CSDMS Standard Name.
+
         """
         pass
 
     def get_value_at_indices(self, long_var_name, inds):
         """Returns the value of the given variable at a location in the model
         grid.
+
+        Parameters
+        ----------
+        long_var_name : str
+          An input or output variable name, a CSDMS Standard Name.
+        inds : array_like
+          The indices into the variable array.
+
         """
         pass
 
     def set_value(self, long_var_name, src):
         """Specifies a new value for a model variable.
+
+        This is the setter for the model, used to change the model's
+        current state. It accepts, through `src`, a new value for a
+        model variable, with the type, size and rank of `src`
+        dependent on the variable.
+
+        Parameters
+        ----------
+        long_var_name : str
+          An input or output variable name, a CSDMS Standard Name.
+        src : array_like
+          The new value for the specified variable.
+
         """
         pass
 
     def set_value_at_indices(self, long_var_name, inds, src):
         """Specifies a new value for a model variable at a location in the
         model grid.
+
+        Parameters
+        ----------
+        long_var_name : str
+          An input or output variable name, a CSDMS Standard Name.
+        inds : array_like
+          The indices into the variable array.
+        src : array_like
+          The new value for the specified variable.
+
         """
         pass
 
