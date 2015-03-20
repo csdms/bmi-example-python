a1049575be663094fc4296ae269795c5a88a88fb
mcflugen <mcflugen@gmail.com>
2015-03-20 14:17:14 -0600

    Wrap long line.

--- bmi/vars.py
+++ bmi/vars.py
@@ -105,7 +105,8 @@ class BmiVars(object):
 
         See Also
         --------
-        bmi.info.BmiInfo.get_input_var_names : Get *var_name* from this method or from :func:`~bmi.info.BmiInfo.get_output_var_names`.
+        bmi.info.BmiInfo.get_input_var_names : Get *var_name* from this
+            method or from :func:`~bmi.info.BmiInfo.get_output_var_names`.
 
         Notes
         -----
