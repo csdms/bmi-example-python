767ae274f62b0a8d08ea9c73147c6db26282524d
mcflugen <mcflugen@gmail.com>
2015-03-20 12:14:36 -0600

    Added figure of a uniform rectilinear grid.

--- bmi/grid_uniform_rectilinear.py
+++ bmi/grid_uniform_rectilinear.py
@@ -2,8 +2,25 @@
 
 
 class BmiGridUniformRectilinear(object):
-    """Defines an interface for converting a standalone model into an
-    integrated modeling framework component.
+    """Methods that describe a uniform rectilinear grid.
+
+    In a 2D uniform grid, every grid cell (or element) is a rectangle and all
+    cells have the same dimensions. If the dimensions are equal, then the
+    grid is a tiling of squares.
+
+    Each of these functions returns information about each dimension of a
+    grid. The dimensions are ordered with "ij" indexing (as opposed to "xy").
+    For example, the :func:`get_grid_shape` function for the example grid would
+    return the array ``[4, 5]``. If there were a third dimension, the length of
+    the z dimension would be listed first. This same convention is used in
+    NumPy. Note that the grid shape is the number of nodes in the coordinate
+    directions and not the number of cells or elements. It is possible for
+    grid values to be associated with the nodes or with the cells.
+
+    .. figure:: _static/grid_uniform_rectilinear.png
+        :scale: 10%
+        :align: center
+        :alt: An example of a uniform rectilinear grid
     """
 
     def get_grid_shape(self, grid_id):
