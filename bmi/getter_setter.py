ce8218b4dd085f7e47d06e73a8de0ceb3e903859
mcflugen <mcflugen@gmail.com>
2015-03-20 11:14:17 -0600

    Added C bindings.

--- bmi/getter_setter.py
+++ bmi/getter_setter.py
@@ -2,33 +2,40 @@
 
 
 class BmiGetter(object):
-    """Defines an interface for converting a standalone model into an
-    integrated modeling framework component.
+    """Get values from a component.
+    
+    Methods that get variables from a model's state. Often a model's state
+    variables are changing with each time step, so getters are called to get
+    current values.
     """
 
-
-    def get_value(self, long_var_name):
-        """Returns the value of the given variable.
+    def get_value(self, var_name):
+        """Get a copy of values of the given variable.
 
         This is a getter for the model, used to access the model's
-        current state. It returns a copy of a model variable, with
+        current state. It returns a *copy* of a model variable, with
         the return type, size and rank dependent on the variable.
 
         Parameters
         ----------
-        long_var_name : str
+        var_name : str
           An input or output variable name, a CSDMS Standard Name.
 
         Returns
         -------
-        object
+        array_like
           The value of a model variable.
 
+        Notes
+        -----
+        .. code-block:: c
+
+            int get_value(void * self, const char * var_name, void * buffer);
         """
         pass
 
-    def get_value_ref(self, long_var_name):
-        """Returns a given variable.
+    def get_value_ref(self, var_name):
+        """Get a reference to values of the given variable.
 
         This is a getter for the model, used to access the model's
         current state. It returns a reference to a model variable,
@@ -36,67 +43,92 @@ class BmiGetter(object):
 
         Parameters
         ----------
-        long_var_name : str
+        var_name : str
           An input or output variable name, a CSDMS Standard Name.
 
         Returns
         -------
-        object
-          A model variable.
+        array_like
+          A reference to a model variable.
 
+        Notes
+        -----
+        .. code-block:: c
+
+            int get_value_ref(void * self, const char * var_name, void ** buffer);
         """
         pass
 
-    def get_value_at_indices(self, long_var_name, inds):
-        """Returns the value of the given variable at a location in the model
-        grid.
-
+    def get_value_at_indices(self, var_name, indices):
+        """Get values at particular indices.
+        
         Parameters
         ----------
-        long_var_name : str
+        var_name : str
           An input or output variable name, a CSDMS Standard Name.
-        inds : array_like
+        indices : array_like
           The indices into the variable array.
 
+        Returns
+        -------
+        array_like
+            Values at the given indices.
+
+        Notes
+        -----
+        .. code-block:: c
+
+            int get_value_at_indices(void * self, const char * var_name, void * buffer, int * indices, int len);
         """
         pass
 
 
 class BmiSetter(object):
-    """Defines an interface for converting a standalone model into an
-    integrated modeling framework component.
+    """Set values into a component.
+   
+    Methods that set variables of a model's state.
     """
 
-    def set_value(self, long_var_name, src):
+
+    def set_value(self, var_name, src):
         """Specifies a new value for a model variable.
 
         This is the setter for the model, used to change the model's
-        current state. It accepts, through `src`, a new value for a
-        model variable, with the type, size and rank of `src`
+        current state. It accepts, through *src*, a new value for a
+        model variable, with the type, size and rank of *src*
         dependent on the variable.
 
         Parameters
         ----------
-        long_var_name : str
+        var_name : str
           An input or output variable name, a CSDMS Standard Name.
         src : array_like
           The new value for the specified variable.
 
+        Notes
+        -----
+        .. code-block:: c
+
+            int set_value(void * self, const char * var_name, void * src);
         """
         pass
 
-    def set_value_at_indices(self, long_var_name, inds, src):
-        """Specifies a new value for a model variable at a location in the
-        model grid.
+    def set_value_at_indices(self, var_name, inds, src):
+        """Specifies a new value for a model variable at particular indices.
 
         Parameters
         ----------
-        long_var_name : str
+        var_name : str
           An input or output variable name, a CSDMS Standard Name.
         inds : array_like
           The indices into the variable array.
         src : array_like
           The new value for the specified variable.
 
+        Notes
+        -----
+        .. code-block:: c
+
+            int set_value_at_indices(void * self, const char * var_name, int * indices, int len, void * src);
         """
         pass
