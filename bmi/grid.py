84176e618d0b9985722d975c4a954bc5daa34903
Mark Piper <mark.piper@colorado.edu>
2015-09-04 13:25:14 -0600

    Add get_grid_size method

--- bmi/grid.py
+++ bmi/grid.py
@@ -34,6 +34,32 @@ class BmiGrid(object):
         """
         pass
 
+    def get_grid_size(self, grid_id):
+        """Get the total number of elements in the computational grid.
+
+        Parameters
+        ----------
+        grid_id : int
+          A grid identifier.
+
+        Returns
+        -------
+        int
+          Size of the grid.
+
+        See Also
+        --------
+        bmi.vars.BmiVars.get_var_grid : Obtain a `grid_id`.
+
+        Notes
+        -----
+        .. code-block:: c
+
+            /* C */
+            int get_grid_size(void * self, int grid_id, int * size);
+        """
+        pass
+
     def get_grid_type(self, grid_id):
         """Get the grid type as a string.
 
