eba882dd602475dea6f95acf4f91cb7d58e53755
mcflugen <mcflugen@gmail.com>
2019-04-16 10:18:19 -0600

    chore: add classifiers

--- setup.py
+++ setup.py
@@ -3,12 +3,26 @@ from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
 
+import versioneer
+
 
 setup(name='bmi-heat',
-      version='0.1.0',
+      version=versioneer.get_version(),
       author='Eric Hutton',
       author_email='eric.hutton@colorado.edu',
       description='BMI Python example',
       long_description=open('README.md').read(),
+      classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering :: Physics",
+      ],
       packages=find_packages(),
+      cmdclass=versioneer.get_cmdclass(),
 )
