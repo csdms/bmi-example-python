0182b2a6a35532f7b3fd553940032b3e5c8143ef
Mark Piper <mark.piper@colorado.edu>
2022-06-20 15:28:57 -0600

    Expose Heat model through heat package

--- heat/__init__.py
+++ heat/__init__.py
@@ -2,7 +2,7 @@
 import pkg_resources
 
 from .bmi_heat import BmiHeat
-from .heat import solve_2d
+from .heat import Heat, solve_2d
 
-__all__ = ["BmiHeat", "solve_2d"]
+__all__ = ["BmiHeat", "solve_2d", "Heat"]
 __version__ = pkg_resources.get_distribution("bmi-heat").version
