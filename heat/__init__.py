44c7d4c54f7fa4c17f51f86a645ba3e2633a4dfb
Mark Piper <mark.piper@colorado.edu>
2022-06-27 10:50:33 -0600

    Add example notebooks (#25)
    * Expose Heat model through heat package
    
    * Add example notebooks
    
    * List examples directory and update build instructions
    
    * Fix typo
    
    * Run black, isort, and flake8

--- heat/__init__.py
+++ heat/__init__.py
@@ -1,6 +1,6 @@
 """Model the diffusion of heat over a 2D plate."""
 from ._version import __version__
 from .bmi_heat import BmiHeat
-from .heat import solve_2d
+from .heat import Heat, solve_2d
 
-__all__ = ["__version__", "BmiHeat", "solve_2d"]
+__all__ = ["__version__", "BmiHeat", "solve_2d", "Heat"]
